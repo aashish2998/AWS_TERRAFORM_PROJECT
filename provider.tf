e95dc00aea900b0d38c432cdbc94a250a1d63833
aashish2998 <aashishkushwaha056@gmail.com>
2024-12-27 12:55:44 +0530

    pushing the code to repo

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
+// Setting up a infrastructure on AWS using Terraform.
